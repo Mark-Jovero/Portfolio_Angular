01fa1405d528b94b0ec9e5c8360656c70a04f86d
Mark Jovero <zmarkjovero@gmail.com>
2023-05-26 14:20:57 -0700

    Added ability to use iframe tag in text editor

--- src/app/components/posts/post-editor/post-editor.component.ts
+++ src/app/components/posts/post-editor/post-editor.component.ts
@@ -14,6 +14,7 @@ import { Header1Component } from '../tags/header1.component';
 import { ItalicsComponent } from '../tags/italics.component';
 import { Tag } from '../tags/tag_interface';
 import { Router } from '@angular/router';
+import { iFrameComponent } from '../tags/iframe.component';
 
 @Component({
   selector: 'app-post-editor',
@@ -51,7 +52,7 @@ export class PostEditorComponent {
 
   hasXSSAlert: boolean = false;
 
-  tags: Array<Tag> = [new ItalicsComponent(), new BoldComponent(), new DivComponent(), new AnchorComponent(), new BreakComponent(), new ImageComponent(null),
+  tags: Array<Tag> = [new iFrameComponent(), new ItalicsComponent(), new BoldComponent(), new DivComponent(), new AnchorComponent(), new BreakComponent(), new ImageComponent(null),
     new Header1Component()];
   tagError: Array<string> = [];
 
---
+++ src/app/components/posts/tags/iframe.component.ts
@@ -0,0 +1,25 @@
+import { Tag } from './tag_interface';
+
+export class iFrameComponent implements Tag{
+  letter: string = 'iframe';
+  tag: string = '<iframe>';
+  end: string = '</iframe>';
+  hasSeparateEndTag: boolean = true;
+
+  allowedAttributes = ['style', 'width', 'height']
+
+  getTag(): string {
+    return this.tag;
+  }
+
+  getEnd(): string {
+    return this.end;
+  }
+
+  containsAttribute(attrb: string): boolean {
+    if (this.allowedAttributes.some(i => i === attrb)) {
+      return true;
+    }
+    return false;
+  };
+}
